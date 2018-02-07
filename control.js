295fb6215671c64045361aac2907af25a8abb6d4
nihadefendi <nihadefendi>
2018-02-06 23:00:03 -0700

    ders 2 basladi

---
+++ control.js
@@ -0,0 +1,13 @@
+var a=3;
+var b=5;
+var y=(a<b);
+if(5<10 || 5 !==4 ){
+    switch (a) {
+        case 'selim':
+            str='Salam Selim';
+            break;
+        default:
+            str='tapilmadi'
+    }
+}
+document.writeln(str);
---
+++ foundamental.js
@@ -0,0 +1,22 @@
+var a,n,b,c,x,y,z;
+a='green';// string
+n='0909'; // numbers
+b=true;   // boolean
+c=['jck','hebele','hubele','atma']; //array
+x=y='green';
+document.write(a+c[3]+n+'<hr>');
+var str='nihad "bura" bax';
+console.log(str.length);
+text.substring(2,4);
+
+// Tarix Funksiyaları
+
+var str = new string();
+var boo = new Boolean();
+var num = new Number();
+var tar = new Date();
+document.writeln('<hr>');
+document.write(tarix.getDay());
+saniye = tarix.getSeconds();
+salise = tarix.getMilliseconds();
+document.write(saat+':'+saniye);
